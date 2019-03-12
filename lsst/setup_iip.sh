a3816b0341cf2891429b84474df0d144b5868e40
Htut Khine <htutkhwin@gmail.com>
2019-03-12 10:35:14 -0500

    Config bug updated.

--- lsst/setup_iip.sh
+++ lsst/setup_iip.sh
@@ -1,6 +1,7 @@
+MYUSER=DM
 mkdir -p /opt/lsst/dm-prompt/bin
 mkdir -p /opt/lsst/dm-prompt/python/logs
 mkdir -p /opt/lsst/dm-prompt/config
 
-cp /home/$USER/src/git/ctrl_iip/python/lsst/iip/* /opt/lsst/dm-prompt/python
-cp /home/$USER/src/git/ctrl_iip/config/L1SystemCfg.yaml /opt/lsst/dm-prompt/config
+cp /home/$MYUSER/src/git/ctrl_iip/python/lsst/iip/* /opt/lsst/dm-prompt/python
+cp /home/$MYUSER/src/git/ctrl_iip/etc/config/L1SystemCfg.yaml /opt/lsst/dm-prompt/config
